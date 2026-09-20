7dbe743b8d25c253d1d3a35f0c2223b8d8854fbe
NIDIGONDA KRUTHIK ROSHAN <krupanidigonda@gmail.com>
2026-09-20 19:35:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 let users = []; // temporary storage
 
